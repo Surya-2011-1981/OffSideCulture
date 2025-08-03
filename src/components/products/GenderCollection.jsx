eca6db2a2bd3e80f29dddd8aa4f3ee9a618f49f8
Surya <135094097+Surya-2011-1981@users.noreply.github.com>
2025-08-03 16:38:08 +0530

    Update GenderCollection.jsx

--- src/components/products/GenderCollection.jsx
+++ src/components/products/GenderCollection.jsx
@@ -5,7 +5,7 @@ const GenderCollection = () => {
     return <section className="py-10 !px-12 lg:px-0">
         <div className="container mx-auto flex flex-col md:flex-row gap-8 ">
             <div className="flex-1 relative">
-                <img src={womensCollection} alt="Women's Collection" className="w-[90%] h-[700px] object-top rounded-sm" />
+                <img src={womensCollection} alt="Women's Collection" className="w-[90%] h-[500px] md:h-[700px] object-top rounded-sm" />
                 <div className="absolute bottom-8 left-2 bg-white bg-opacity-90 p-4 rounded-sm">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
                         Women's Collection
@@ -15,7 +15,7 @@ const GenderCollection = () => {
             </div>
             {/* Mens Collection */}
             <div className="flex-1 relative">
-                <img src={mensCollection} alt="Wonen's Collection" className="w-[90%] h-[700px] object-top rounded-sm " />
+                <img src={mensCollection} alt="Wonen's Collection" className="w-[90%] h-[500px] md:h-[700px] object-top rounded-sm " />
                 <div className="absolute bottom-8 left-2 bg-white bg-opacity-90 p-4 rounded-sm">
                     <h2 className="text-2xl font-bold text-gray-900 mb-3">
                         Men's Collection
